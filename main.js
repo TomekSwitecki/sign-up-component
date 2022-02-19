74b7e060d36867b61908217edffba01fbb471591
Tomek Świtecki <99132195+TomekSwitecki@users.noreply.github.com>
2022-02-19 19:14:50 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+function validation() {
+
+    var email = $("#email-input");
+    var password = $("#password-input");
+    var fname = $("#fname-input");
+    var lname = $("#lname-input");
+    var red='hsl(0, 100%, 74%)';
+    var dark_blue='hsl(249, 10%, 26%)';
+    if (email.val() === "" || !validateEmail(email.val())) {
+        Show_error("#email",red);
+    }
+    else {
+        Hide_error("#email",dark_blue);
+    }
+
+
+    if (password.val() === "") {
+        Show_error("#password",red);
+    }
+    else {
+        Hide_error("#password",dark_blue);
+    }
+
+
+    if (fname.val() === "") {
+        Show_error("#fname",red);
+    }
+    else {
+        Hide_error("#fname",dark_blue);
+    }
+
+    if (lname.val() === "") {
+        Show_error("#lname",red);
+    }
+    else {
+        Hide_error("#lname",dark_blue);
+    }
+
+}
+
+
+function Show_error(input,color)
+{
+
+        $(input+"-error-icon").css("visibility", "visible");
+        $(input+"-error-text").css("visibility", "visible");
+        $(input).css('border-color', color);
+}
+
+function Hide_error(input,color)
+{
+
+        $(input+"-error-icon").css("visibility", "hidden");
+        $(input+"-error-text").css("visibility", "hidden");
+        $(input).css('border-color', color);
+}
+
+
+
+
+$(".btn-green").click(function () {
+    validation();
+});
+
+ function validateEmail($email) {
+  var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+  return emailReg.test( $email );
+}
